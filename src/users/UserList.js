9887bb1d026d98d216c6c1310ed5ca8b7941699b
Mujib <mujib517@gmail.com>
2022-03-04 08:51:45 +0530

    Error handling

--- src/users/UserList.js
+++ src/users/UserList.js
@@ -8,18 +8,18 @@ import Loader from '../utils/Loader';
 // presentation
 class UserList extends React.Component {
 
-    state = { users: [], loading: true };
+    state = { users: [], loading: true, error: false };
 
     constructor() {
         super();
         setTimeout(() => {
-            axios.get('https://api.github.com/users')
+            axios.get('https://api.github.com/users1')
                 .then(res => {
                     const data = res.data;
-                    this.setState({ loading: false, users: data });
+                    this.setState({ loading: false, users: data, error: false });
                 })
                 .catch(err => {
-                    this.setState({ loading: false });
+                    this.setState({ loading: false, error: true });
                     console.log(err);
                 });
         }, 1000);
@@ -30,6 +30,11 @@ class UserList extends React.Component {
             <h1>Users</h1>
             <ShouldRender cond={this.state.loading}>
                 <Loader />
+            </ShouldRender>
+            <ShouldRender cond={this.state.error}>
+                <div className="col-md-8">
+                    <div class="alert alert-danger">Something went wrong. Try again</div>
+                </div>
             </ShouldRender>
             {this.state.users.map(user => <User key={user.login} user={user} />)}
         </div>
