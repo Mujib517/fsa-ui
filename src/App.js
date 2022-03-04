57becb3ad120c03f2abcc8e26c78c1fced7bbb5f
Mujib <mujib517@gmail.com>
2022-03-04 08:44:38 +0530

    Loading gif

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './Header';
 import Footer from './Footer';
 import 'bootstrap/dist/css/bootstrap.css';
 import UserList from './users/UserList';
+import './App.css';
 
 function App() {
     return <div>
--- src/users/UserList.js
+++ src/users/UserList.js
@@ -1,29 +1,39 @@
 import React from 'react';
 import axios from 'axios';
 import User from './User';
+import LoaderImg from '../img/loader.gif';
 
 // container 
 // presentation
 class UserList extends React.Component {
 
-    state = { users: [] };
+    state = { users: [], loading: true };
 
     constructor() {
         super();
-
         setTimeout(() => {
             axios.get('https://api.github.com/users')
                 .then(res => {
                     const data = res.data;
-                    this.setState({ users: data });
+                    this.setState({ loading: false, users: data });
                 })
-                .catch(err => console.log(err));
+                .catch(err => {
+                    this.setState({ loading: false });
+                    console.log(err);
+                });
         }, 1000);
     }
 
     render() {
         return <div>
             <h1>Users</h1>
+            {
+                this.state.loading &&
+                <div className="loader">
+                    <img src={LoaderImg} />
+                    Loading...
+                </div>
+            }
             {this.state.users.map(user => <User key={user.login} user={user} />)}
         </div>
     }
