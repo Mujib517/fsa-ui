194269116505f43de68857103f8a5c521da7b2e8
Mujib <mujib517@gmail.com>
2022-03-15 08:50:10 +0530

    Mapping key and values with enums

--- src/users/User.js
+++ src/users/User.js
@@ -1,14 +1,30 @@
 import UserIcon from '../img/user-icon.jpeg';
 
+const Degree = [
+    'BE/BTech',
+    'BCom',
+    'BSc',
+    'Others'
+]
+
+const Qualification = [
+    '10+2',
+    'UG',
+    'PG',
+    'Others'
+];
+
 const User = ({ user }) => {
     return <div className="m-3 col-md-3">
         <div className="card">
             <img width="150" height="220" className="card-img-top" src={UserIcon}></img>
             <div className="card-header">{user.firstName} {user.lastName}</div>
             <div className="card-body">
-                <div>{user.email}</div>
-                <div>{user.degree}</div>
-                <div>{user.qualification}</div>
+                <div>
+                    <i className="fa fa-envelope"></i>   {user.email}
+                </div>
+                <div><b>{Degree[user.degree]}</b></div>
+                <b>{Qualification[user.qualification]}</b>
             </div>
             <div className="card-footer">
                 <button className="btn btn-danger">
