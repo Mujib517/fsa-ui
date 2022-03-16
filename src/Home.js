4f9549807a0a7db5ef976f676211e4dcd7ff6384
Mujib <mujib517@gmail.com>
2022-03-16 09:03:39 +0530

    Context api

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,36 @@
-import UpdateUser from "./UpdateUser";
+import React, { useContext, useState } from "react"
+
+const DataContext = React.createContext('data');
+
+const Level3 = () => {
+    const cnt = useContext(DataContext);
+    return <>
+        <h1>Level 3, count is {cnt}</h1>
+    </>
+}
+
+const Level2 = () => {
+    const cnt = useContext(DataContext);
+    return <>
+        <h1>Level 2, {cnt}</h1>
+        <Level3 />
+    </>
+}
+const Level1 = () => <>
+    <h1>Level 1</h1>
+    <Level2 />
+</>
+
+
+const Root = () => {
+    return <DataContext.Provider value={10000000}>
+        <Level1 />
+    </DataContext.Provider>
+}
 
 const Home = () => <>
     <h1>Home Page</h1>
+    <Root />
 </>
 
 export default Home;
